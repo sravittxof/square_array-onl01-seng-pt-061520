1ef75ec02be32bda81eb60b2292922f07f381912
Travis Fox <travis.fox90@gmail.com>
2020-06-22 01:39:14 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |number|
+    squared_array.push(number**2)
+  end
+  return squared_array
 end
+
+def square_array_with_collect(new_array)
+  new_array.collect { |new_number| new_number**2 }
+end
+
